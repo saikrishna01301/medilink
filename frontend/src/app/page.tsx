5b5a16d90a880cd440ba3a5801d14bed661d82b0
Abhineet Chaudhary <38350338+Virtual-Viking@users.noreply.github.com>
2025-09-27 01:45:03 -0400

    importing image assets

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      { <p>We will build our design here</p> }
+      {
+        <header>
+          <img src=''
+        </header>
+      }
     </main>
   );
 }
